135ee57375767ffe19244d5195ddac9258519883
Entwickler7aSv <entwickler7asv@yahoo.com>
2018-12-26 13:50:55 +0300

    Add sendMessage declare

--- src/api.ts
+++ src/api.ts
@@ -1,29 +1,15 @@
 import { SnowFlake } from "./index";
+
 export interface SendMessage {
-    /**
-     * The message contents (up to 2000 characters)
-     */
     content: string;
 
-    /**
-     * a nonce that can be used for optimistic message sending
-     */
     nonce: SnowFlake;
 
-    /**
-     * true if this is a TTS message
-     */
     tts: boolean;
 
-    /**
-     * The contents of the file being sent
-     */
     file: File;
 
-    // TODO Embed
-
-    /**
-     * JSON encoded body of any additional request fields.
-     */
     payload_json: string;
+
+    // TODO Embed
 }
